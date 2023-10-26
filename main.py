d00dbb8c12a7b5e17ea8b75ea8c4c2a44b95a981
demiapollo <apollodemirel67@gmail.com>
2023-10-25 18:36:52 -0700

    completed

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import colorgram
-
+#import colorgram
 # Extracting 10 color from the downloaded image
 # rgb_colors = []
 # colors = colorgram.extract("image.jpg", 30)
-
 # for color in colors:
 #     r = color.rgb.r
 #     g = color.rgb.g
 #     b = color.rgb.b
 #     new_color = (r, g, b)
 #     rgb_colors.append(new_color)
-
 # print(rgb_colors)
 
 color_list = [(236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), 
@@ -18,4 +15,34 @@ color_list = [(236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (1
  (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), 
  (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
     
+from turtle import Screen
+import turtle as t
+import random
 
+tim = t.Turtle()
+t.colormode(255)
+
+tim.penup()
+tim.hideturtle()
+tim.setheading(225)
+tim.forward(250)
+tim.setheading(0)
+tim.speed("fastest")
+
+number_of_dots = 100
+
+# Turtle creates the dots
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen = Screen()
+screen.exitonclick()
